359b16e0a58e9b13df5ed61b8d89f7ddc9ed7a12
Ashley Conner <108750120+valardohaeris2@users.noreply.github.com>
2024-08-09 11:21:52 -0400

    Create crypto.py

---
+++ crypto.py
@@ -0,0 +1,22 @@
+import requests
+from twilio.rest import Client
+
+STOCK_ENDPOINT = "https://www.alphavantage.co/query/"
+NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
+
+STOCK_API_KEY = ""
+NEWS_API_KEY = ""
+TWILIO_SID = ""
+TWILIO_AUTH_TOKEN = ""
+
+crypto_params = {
+    "function": "DIGITAL_CURRENCY_DAILY",
+    "symbol": "ETH",
+    "market": "USD",
+    "apikey": STOCK_API_KEY,
+}
+
+response = requests.get(STOCK_ENDPOINT, params=crypto_params)
+data = response.json()
+
+print(data)
